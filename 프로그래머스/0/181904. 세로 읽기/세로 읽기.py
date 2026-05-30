e2e4005c76b15b25c6ae236c18c1f91a8ac4fe49
Mijeong Kwon <kmj.dozhaldo0321@gmail.com>
2026-05-30 15:43:07 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -0,0 +1,11 @@
+def solution(my_string, m, c) : 
+    answer = ''
+    str = []
+
+    for i in range(len(my_string)//m):
+        str.append(my_string[i*m : i*m + m])
+
+    for i in range(len(str)) :
+        answer += str[i][c-1]
+
+    return answer
