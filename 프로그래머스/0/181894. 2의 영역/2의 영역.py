73a6fb83d5268d9cdbf7b484e2f2646ccb2094ab
Mijeong Kwon <kmj.dozhaldo0321@gmail.com>
2026-05-30 15:50:08 +0900

    [level 0] Title: 2의 영역, Time: 2.65 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    check = []
+    if 2 not in arr : 
+        return [-1]
+    else : 
+        for i in range(0, len(arr)) : 
+            if arr[i] == 2 : 
+                check.append(i)
+    return arr[check[0] : check[-1] + 1]
